944dc2c45acd2a483c220d929c468cd95ac5830f
takahi-i <takahikoito@gmail.com>
2018-05-25 14:08:20 +0900

    Support command line option --conf

--- bin/satori-cli.js
+++ bin/satori-cli.js
@@ -1,13 +1,20 @@
 #! /usr/bin/env node
 
-import { ConfigurationBuilder } from 'satori-flow'
+import { Configuration } from 'satori-flow'
 import { IntentDetector } from 'satori-flow'
+const Command = require("commander").Command;
+const fs = require("fs");
+const program = new Command()
 
-const config = new ConfigurationBuilder()
-    .addIntent("repeat", { "type" : "verbatim", "patterns" : [ "repeat", "please repeat" ]})
-    .addRunner("repeat", {"type" : "random", "list" : ["sure"]} )
-    .build();
+program
+    .version('0.1.1')
+    .option('-c, --config [file_path]', 'Add configuration file')
+    .parse(process.argv);
 
-const detector = new IntentDetector(config);
-const result = detector.match({"text" : "I would like to repeat again.", "userId" : 985499 });
-console.log(result)
+console.log('config path %s', program.config);
+fs.readFile(program.config, 'utf-8', function (err, data) {
+    const configuration = new Configuration(data);
+    const detector = new IntentDetector(configuration);
+    const result = detector.match({"text" : "I would like to repeat again.", "userId" : 985499 });
+    console.log(result);
+});
