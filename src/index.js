75df9b9ff981c1dca526a0d8de179476dca53212
takahi-i <takahikoito@gmail.com>
2018-05-28 17:37:01 +0900

    Apply DialogueManager

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ export { default as IntentDetector } from "./intent_detector";
 export { default as RandomRunner } from "./runner/random_runner";
 export { default as ConfigurationBuilder } from "./configuration_builder"
 export { default as Configuration } from "./configuration"
-export { default as DialogueManger } from "./dialogue_manager"
+export { default as DialogueManager } from "./dialogue_manager"
